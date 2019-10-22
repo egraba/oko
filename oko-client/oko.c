cde4be427df94994a9ee4e812182a281b421e166
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2019-10-22 20:04:35 +0200

    Retrieve IP v4 & v6

--- oko-client/oko.c
+++ oko-client/oko.c
@@ -1,3 +1,6 @@
+#include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
 #include <sys/sysctl.h>
 #include <math.h>
 #include <stdio.h>
@@ -50,6 +53,68 @@ retrieve_hostname(machine *machine)
     }
     machine->hostname = malloc(len);
     rc = sysctlbyname("kern.hostname", machine->hostname, &len, NULL, 0);
+    
+    return (rc);
+}
+
+static int
+retrieve_ipv4(machine *machine)
+{
+    int rc;
+    struct ifaddrs *ifa;
+    struct ifaddrs *ifs;
+    struct sockaddr_in *ipv4;
+    char *ip_if;
+
+    ipv4 = malloc(sizeof(struct sockaddr_in));
+    rc = getifaddrs(&ifs);
+    if (rc != 0) {
+        return (rc);
+    }
+    ip_if = strdup("en0");
+    
+    rc = -1;
+    for (ifa = ifs; ifa != NULL; ifa = ifa->ifa_next) {
+        if (!strcmp(ifa->ifa_name, ip_if)) {
+            if (ifa->ifa_addr->sa_family == AF_INET) {
+                ipv4->sin_addr.s_addr = ((struct sockaddr_in*)(ifa->ifa_addr))->sin_addr.s_addr;
+                machine->ipv4 = malloc(INET_ADDRSTRLEN);
+                strncpy(machine->ipv4, inet_ntoa(ipv4->sin_addr), INET_ADDRSTRLEN);
+                rc = 0;
+            }
+        }
+    }
+    
+    return (rc);
+}
+
+static int
+retrieve_ipv6(machine *machine)
+{
+    int rc;
+    struct ifaddrs *ifa;
+    struct ifaddrs *ifs;
+    struct sockaddr_in6 *ipv6;
+    char *ip_if;
+
+    ipv6 = malloc(sizeof(struct sockaddr_in6));
+    rc = getifaddrs(&ifs);
+    if (rc != 0) {
+        return (rc);
+    }
+    ip_if = strdup("en0");
+    
+    rc = -1;
+    for (ifa = ifs; ifa != NULL; ifa = ifa->ifa_next) {
+        if (!strcmp(ifa->ifa_name, ip_if)) {
+            if (ifa->ifa_addr->sa_family == AF_INET6) {
+                ipv6->sin6_addr = ((struct sockaddr_in6*)(ifa->ifa_addr))->sin6_addr;
+                machine->ipv6 = malloc(INET6_ADDRSTRLEN);
+                inet_ntop(AF_INET6, (const void *)&ipv6->sin6_addr, machine->ipv6, INET6_ADDRSTRLEN);
+                rc = 0;
+            }
+        }
+    }
     
     return (rc);
 }
@@ -176,8 +241,10 @@ collect_info(machine *machine)
     nerrors += retrieve_hostname(machine);
     
     /* ipv4 */
+    nerrors += retrieve_ipv4(machine);
     
     /* ipv6 */
+    nerrors += retrieve_ipv6(machine);
     
     /* mac_address */
     
