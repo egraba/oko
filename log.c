1c73f58c52436fd96e7f51fa14f797e77163886b
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-07-17 11:56:45 +0200

    Fix refresh on logs

--- log.c
+++ log.c
@@ -5,87 +5,126 @@
 #include "cJSON.h"
 
 void
-log_machine_info(machine *machine)
+init_machine_info_json(machine *machine)
 {
-	cJSON *json_machine = cJSON_CreateObject();
+	json_machine = cJSON_CreateObject();
 	
-	cJSON *serialnumber = cJSON_CreateString(machine->serialnumber);
+	serialnumber = cJSON_CreateString(machine->serialnumber);
 	cJSON_AddItemToObject(json_machine, "serialnumber", serialnumber);
-	cJSON *type = cJSON_CreateString(machine->type);
+	type = cJSON_CreateString(machine->type);
 	cJSON_AddItemToObject(json_machine, "type", type);
-	cJSON *model = cJSON_CreateString(machine->model);
+	model = cJSON_CreateString(machine->model);
 	cJSON_AddItemToObject(json_machine, "model", model);
-	cJSON *hostname = cJSON_CreateString(machine->hostname);
+	hostname = cJSON_CreateString(machine->hostname);
 	cJSON_AddItemToObject(json_machine, "hostname", hostname);
 	
-	cJSON *network = cJSON_CreateObject();
+	network = cJSON_CreateObject();
 	cJSON_AddItemToObject(json_machine, "network", network);
 	
-	cJSON *ip = cJSON_CreateString(machine->network.ip);
+	ip = cJSON_CreateString(machine->network.ip);
 	cJSON_AddItemToObject(network, "ip", ip);
-	cJSON *macaddress = cJSON_CreateString(machine->network.macaddress);
+	macaddress = cJSON_CreateString(machine->network.macaddress);
 	cJSON_AddItemToObject(network, "macaddress", macaddress);
 
-	cJSON *cpu = cJSON_CreateObject();
+	cpu = cJSON_CreateObject();
 	cJSON_AddItemToObject(json_machine, "cpu", cpu);
 	
-	cJSON *arch = cJSON_CreateString(machine->cpu.arch);
-	cJSON_AddItemToObject(cpu, "arch", arch);
-	cJSON *cpu_model = cJSON_CreateString(machine->cpu.model);
+	cpu_arch = cJSON_CreateString(machine->cpu.arch);
+	cJSON_AddItemToObject(cpu, "arch", cpu_arch);
+	cpu_model = cJSON_CreateString(machine->cpu.model);
 	cJSON_AddItemToObject(cpu, "model", cpu_model);
-	cJSON *ncpus = cJSON_CreateNumber(machine->cpu.ncpus);
+	ncpus = cJSON_CreateNumber(machine->cpu.ncpus);
 	cJSON_AddItemToObject(cpu, "ncpus", ncpus);
 
-	cJSON *memory = cJSON_CreateObject();
+	memory = cJSON_CreateObject();
 	cJSON_AddItemToObject(json_machine, "memory", memory);
 	
-	cJSON *physmem = cJSON_CreateNumber(machine->memory.physmem);
+	physmem = cJSON_CreateNumber(machine->memory.physmem);
 	cJSON_AddItemToObject(memory, "physmem", physmem);
 
-	cJSON *os = cJSON_CreateObject();
+	os = cJSON_CreateObject();
 	cJSON_AddItemToObject(json_machine, "os", os);
 	
-	cJSON *name = cJSON_CreateString(machine->os.name);
+	name = cJSON_CreateString(machine->os.name);
 	cJSON_AddItemToObject(os, "name", name);
-	cJSON *release = cJSON_CreateString(machine->os.release);
+	release = cJSON_CreateString(machine->os.release);
 	cJSON_AddItemToObject(os, "release", release);
-	cJSON *version = cJSON_CreateString(machine->os.version);
+	version = cJSON_CreateString(machine->os.version);
 	cJSON_AddItemToObject(os, "version", version);
+}
+
+void
+log_machine_info(machine *machine)
+{
+	cJSON_SetValuestring(serialnumber, machine->serialnumber);
+	cJSON_SetValuestring(type, machine->type);
+	cJSON_SetValuestring(model, machine->model);
+	cJSON_SetValuestring(hostname, machine->hostname);
+	
+	ip = cJSON_CreateString(machine->network.ip);
+	cJSON_AddItemToObject(network, "ip", ip);
+	macaddress = cJSON_CreateString(machine->network.macaddress);
+	cJSON_AddItemToObject(network, "macaddress", macaddress);
+	
+	cJSON_SetValuestring(cpu_arch, machine->cpu.arch);
+	cJSON_SetValuestring(cpu_model, machine->cpu.model);
+	cJSON_SetNumberValue(ncpus, machine->cpu.ncpus);
+	
+	cJSON_SetNumberValue(physmem, machine->memory.physmem);
+	
+	cJSON_SetValuestring(name, machine->os.name);
+	cJSON_SetValuestring(release, machine->os.release);
+	cJSON_SetValuestring(version, machine->os.version);
 	
 	printf("%s\n", cJSON_PrintUnformatted(json_machine));
 }
 
 void
-log_machine_usage(usage *usage)
+init_machine_usage_json(usage *usage)
 {
-	cJSON *json_usage = cJSON_CreateObject();
+	json_usage = cJSON_CreateObject();
 	
-	cJSON *cpu = cJSON_CreateObject();
+	cpu = cJSON_CreateObject();
 	cJSON_AddItemToObject(json_usage, "cpu", cpu);
 
-	cJSON *user = cJSON_CreateNumber(usage->cpu.user);
-	cJSON_AddItemToObject(cpu, "user", user);
-	cJSON *system = cJSON_CreateNumber(usage->cpu.system);
-	cJSON_AddItemToObject(cpu, "system", system);
-	cJSON *idle = cJSON_CreateNumber(usage->cpu.idle);
-	cJSON_AddItemToObject(cpu, "idle", idle);
-	cJSON *nice = cJSON_CreateNumber(usage->cpu.nice);
-	cJSON_AddItemToObject(cpu, "nice", nice);
+	cpu_user = cJSON_CreateNumber(usage->cpu.user);
+	cJSON_AddItemToObject(cpu, "user", cpu_user);
+	cpu_system = cJSON_CreateNumber(usage->cpu.system);
+	cJSON_AddItemToObject(cpu, "system", cpu_system);
+	cpu_idle = cJSON_CreateNumber(usage->cpu.idle);
+	cJSON_AddItemToObject(cpu, "idle", cpu_idle);
+	cpu_nice = cJSON_CreateNumber(usage->cpu.nice);
+	cJSON_AddItemToObject(cpu, "nice", cpu_nice);
 
-	cJSON *memory = cJSON_CreateObject();
+	memory = cJSON_CreateObject();
 	cJSON_AddItemToObject(json_usage, "memory", memory);
 
-	cJSON *used = cJSON_CreateNumber(usage->memory.used);
-	cJSON_AddItemToObject(memory, "used", used);
-	cJSON *free = cJSON_CreateNumber(usage->memory.free);
-	cJSON_AddItemToObject(memory, "free", free);
-	cJSON *swaptotal = cJSON_CreateNumber(usage->memory.swaptotal);
+	memory_used = cJSON_CreateNumber(usage->memory.used);
+	cJSON_AddItemToObject(memory, "used", memory_used);
+	memory_free = cJSON_CreateNumber(usage->memory.free);
+	cJSON_AddItemToObject(memory, "free", memory_free);
+	swaptotal = cJSON_CreateNumber(usage->memory.swaptotal);
 	cJSON_AddItemToObject(memory, "swaptotal", swaptotal);
-	cJSON *swapused = cJSON_CreateNumber(usage->memory.swapused);
+	swapused = cJSON_CreateNumber(usage->memory.swapused);
 	cJSON_AddItemToObject(memory, "swapused", swapused);
-	cJSON *swapfree = cJSON_CreateNumber(usage->memory.swapfree);
+	swapfree = cJSON_CreateNumber(usage->memory.swapfree);
 	cJSON_AddItemToObject(memory, "swapfree", swapfree);
+}
 
+void
+log_machine_usage(usage *usage)
+{
+	cJSON_SetNumberValue(cpu_user, usage->cpu.user);
+	cJSON_SetNumberValue(cpu_system, usage->cpu.system);
+	cJSON_SetNumberValue(cpu_idle, usage->cpu.idle);
+	cJSON_SetNumberValue(cpu_nice, usage->cpu.nice);
+	
+	cJSON_SetNumberValue(memory_used, usage->memory.used);
+	cJSON_SetNumberValue(memory_free, usage->memory.free);
+	cJSON_SetNumberValue(swaptotal, usage->memory.swaptotal);
+	cJSON_SetNumberValue(swapused, usage->memory.swapused);
+	cJSON_SetNumberValue(swapfree, usage->memory.swapfree);
+	
 	printf("%s\n", cJSON_PrintUnformatted(json_usage));
 }
 
@@ -95,10 +134,14 @@ log_mode(int interval)
 	machine m;
 	usage u;
 	
+	init_machine_info_json(&m);
+	init_machine_usage_json(&u);
+
 	collect_machine_info(&m);
 	log_machine_info(&m);
 	
 	for (;;) {
+		collect_machine_usage(&u);
 		log_machine_usage(&u);
 		sleep(interval);
 	}
--- log.h
+++ log.h
@@ -1,8 +1,43 @@
 #ifndef log_h
 #define log_h
 
+#include "cJSON.h"
 #include "oko.h"
 
+cJSON *json_machine;
+cJSON *serialnumber;
+cJSON *type;
+cJSON *model;
+cJSON *hostname;
+cJSON *network;
+cJSON *ip;
+cJSON *macaddress;
+cJSON *cpu;
+cJSON *cpu_arch;
+cJSON *cpu_model;
+cJSON *ncpus;
+cJSON *memory;
+cJSON *physmem;
+cJSON *os;
+cJSON *name;
+cJSON *release;
+cJSON *version;
+
+cJSON *json_usage;
+cJSON *cpu;
+cJSON *cpu_user;
+cJSON *cpu_system;
+cJSON *cpu_idle;
+cJSON *cpu_nice;
+cJSON *memory;
+cJSON *memory_used;
+cJSON *memory_free;
+cJSON *swaptotal;
+cJSON *swapused;
+cJSON *swapfree;
+
+void init_machine_info_json(machine *machine);
+void init_machine_usage_json(usage *usage);
 void log_machine_info(machine *machine);
 void log_machine_usage(usage *usage);
 
