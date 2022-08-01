12fbd6159c0b2bdef08a5e00ea925e71bc5c272b
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-08-01 19:50:01 +0200

    Enhance formatting of minunit framework

--- test/minunit.h
+++ test/minunit.h
@@ -2,7 +2,14 @@
  * This file is the source code of the MinUnit framework. 
  * For more information, see: https://jera.com/techinfo/jtns/jtn002
  */
-#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-#define mu_run_test(test) do { char *message = test(); tests_run++; \
-                                if (message) return message; } while (0)
+#define mu_assert(message, test) do { \
+        if (!(test)) return message;  \
+    } while (0)
+
+#define mu_run_test(test) do {        \
+        char *message = test();       \
+        tests_run++;                  \
+        if (message) return message;  \
+    } while (0)
+
 extern int tests_run;
