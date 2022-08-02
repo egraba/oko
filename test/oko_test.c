13066b5dce0c1b91ce593947c98843acc5cca9ea
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-08-02 23:00:51 +0200

    Enhance the formatting in tests

--- test/oko_test.c
+++ test/oko_test.c
@@ -16,7 +16,6 @@ START_TEST(test_retrieve_serial_number)
     
     retrieve_serialnumber(&m);
     execute("system_profiler SPHardwareDataType | grep Serial | awk '{print $4}'", serialnumber);
-    
     ck_assert_str_eq(m.serialnumber, serialnumber);
 
     free(serialnumber);
@@ -29,8 +28,7 @@ START_TEST(test_retrieve_type)
     char *type = (char *) malloc(10);
     
     retrieve_type(&m);
-    execute("sysctl hw.targettype | awk '{print $2}'", type);
-    
+    execute("sysctl hw.targettype | awk '{print $2}'", type);    
     ck_assert_str_eq(m.type, type);
 
     free(type);
@@ -44,7 +42,6 @@ START_TEST(test_retrieve_model)
 
     retrieve_model(&m);
     execute("system_profiler SPHardwareDataType | grep 'Model Identifier' | awk '{print $3}'", model);
-    
     ck_assert_str_eq(m.model, model);
 
     free(model);
@@ -58,7 +55,6 @@ START_TEST(test_retrieve_hostname)
     
     retrieve_hostname(&m);
     execute("hostname", hostname);
-
     ck_assert_str_eq(m.hostname, hostname);
 
     free(hostname);
@@ -72,7 +68,6 @@ START_TEST(test_retrieve_ip)
 
     retrieve_ip(&m);
     execute("ifconfig | grep \"inet \" | grep -Fv 127.0.0.1 | awk '{print $2}'", ip);
-
     ck_assert_str_eq(m.network.ip, ip);
 
     free(ip);
@@ -86,7 +81,6 @@ START_TEST(test_retrieve_macaddress)
 
     retrieve_macaddress(&m);
     execute("ifconfig en0 | grep \"ether \" | awk '{print $2}'", macaddress);
-
     ck_assert_str_eq(m.network.macaddress, macaddress);
 
     free(macaddress);
@@ -100,7 +94,6 @@ START_TEST(test_retrieve_cpu_arch)
 
     retrieve_cpu_arch(&m);
     execute("sysctl hw.machine | awk '{print $2}'", cpu_arch);
-
     ck_assert_str_eq(m.cpu.arch, cpu_arch);
 
     free(cpu_arch);
@@ -114,7 +107,6 @@ START_TEST(test_retrieve_cpu_model)
 
     retrieve_cpu_model(&m);
     execute("sysctl machdep.cpu.brand_string | awk '{print $2\" \"$3\" \"$4\" \"$5\" \"$6\" \"$7}'", cpu_model);
-
     ck_assert_str_eq(m.cpu.model, cpu_model);
 
     free(cpu_model);
@@ -128,7 +120,6 @@ START_TEST(test_retrieve_ncpus)
 
     retrieve_ncpus(&m);
     execute("sysctl hw.ncpu | awk '{print $2}'", ncpus);
-
     ck_assert_int_eq(m.cpu.ncpus, atoi(ncpus));
 
     free(ncpus);
@@ -142,7 +133,6 @@ START_TEST(test_retrieve_physmem)
     
     retrieve_physmem(&m);
     execute("sysctl hw.memsize | awk '{print $2}'", physmem);
-
     ck_assert_int_eq(m.memory.physmem, atol(physmem));
 
     free(physmem);
@@ -156,7 +146,6 @@ START_TEST(test_retrieve_os_name)
  
     retrieve_os_name(&m);
     execute("uname -s", os_name);
-
     ck_assert_str_eq(m.os.name, os_name);
 
     free(os_name);
@@ -170,7 +159,6 @@ START_TEST(test_retrieve_os_release)
     
     retrieve_os_release(&m);
     execute("uname -r", os_release);
-
     ck_assert_str_eq(m.os.release, os_release);
 
     free(os_release);
@@ -184,7 +172,6 @@ START_TEST(test_retrieve_os_version)
     
     retrieve_os_version(&m);
     execute("uname -v", os_version);
-    
     ck_assert_str_eq(m.os.version, os_version);
 
     free(os_version);
@@ -216,11 +203,9 @@ START_TEST(test_retrieve_memory_usage)
     retrieve_memory_usage(&u);
 
     execute("sysctl vm.pages | awk '{print $2*4096}'", mem_used);
-    
     ck_assert_uint_eq(atol(mem_used), u.memory.used);
 
     execute("sysctl vm.page_free_count | awk '{print $2*4096}'", mem_free);
-    
     /* 5% margin */
     upper_margin = u.memory.free + 0.05 * u.memory.free;
     lower_margin = u.memory.free - 0.05 * u.memory.free;
@@ -249,17 +234,14 @@ START_TEST(test_retrieve_memory_swapusage)
     
     execute("sysctl -a | grep swapusage | awk '{print $4}'", swaptotal);
     sprintf(s, "%.2fM", u.memory.swaptotal / pow(1024, 2));
-
     ck_assert_str_eq(s, swaptotal);
     
     execute("sysctl -a | grep swapusage | awk '{print $7}'", swapused);
     sprintf(s, "%.2fM", u.memory.swapused / pow(1024, 2));
-
     ck_assert_str_eq(s, swapused);
 
     execute("sysctl -a | grep swapusage | awk '{print $10}'", swapfree);
     sprintf(s, "%.2fM", u.memory.swapfree / pow(1024, 2));
-
     ck_assert_str_eq(s, swapfree);
 
     free(swaptotal);
