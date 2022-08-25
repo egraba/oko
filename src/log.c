709cac3b3299f29385f27b86a1489bbd9641c932
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-08-25 15:34:11 +0200

    Remove '@' from timestamp

--- src/log.c
+++ src/log.c
@@ -45,7 +45,7 @@ log_json(char *json_type, char * json_string)
 	time_t t = time(NULL);
 	struct tm tm = *localtime(&t);
 
-	printf("{\"@timestamp\": \"%02d-%02d-%02dT%02d:%02d:%02d\", \"%s\": %s}\n",
+	printf("{\"timestamp\": \"%02d-%02d-%02dT%02d:%02d:%02d\", \"%s\": %s}\n",
 		tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec,
 		json_type, json_string);
 }
