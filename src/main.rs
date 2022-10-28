73c10942019e0931f096904c9b89ad8495d35800
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-10-28 20:42:06 +0200

    Init main structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+/// Agent collecting information about the OS on which it is installed
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    /// Sets refresh interval (in seconds)
+    #[arg(short, long, default_value_t = 2)]
+    interval: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Args::parse();
 }
