fe3bdf8dfc78153725badc86263312b8a6cc7ab4
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-10-30 16:43:54 +0100

    Make the program more modular

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use crate::display::launch_display_mode;
 use clap::Parser;
-use cursive::views::{LinearLayout, Panel, TextView};
 use std::time;
-use sysinfo::{System, SystemExt};
+
+pub mod display;
 
 /// Agent collecting information about the OS on which it is installed
 #[derive(Parser)]
@@ -14,43 +15,7 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    let interval = time::Duration::from_secs(args.interval);
+    let interval = time::Duration::from_secs(args.interval).as_secs();
 
-    let sys = System::new_all();
-
-    let mut siv = cursive::default();
-    siv.load_toml(include_str!("theme.toml")).unwrap();
-
-    const MAX_REFRESH_RATE: u64 = 30;
-    match interval.as_secs() {
-        0..=MAX_REFRESH_RATE => siv.set_fps(interval.as_secs().try_into().unwrap()),
-        _ => siv.set_fps(MAX_REFRESH_RATE.try_into().unwrap()),
-    }
-
-    siv.add_global_callback('q', |s| s.quit());
-
-    let linear_layout = LinearLayout::vertical()
-        .child(TextView::new("oko <o> - Press 'q' to exit."))
-        .child(
-            Panel::new(TextView::new(format!(
-                "os name: {:?}, release: {:?}",
-                sys.name(),
-                sys.os_version()
-            )))
-            .title("Machine"),
-        )
-        .child(
-            Panel::new(TextView::new(format!(
-                "memory used: {:?}, free: {:?}, swapused: {:?}, swapfree: {:?}",
-                sys.used_memory(),
-                sys.free_memory(),
-                sys.used_swap(),
-                sys.free_swap()
-            )))
-            .title("Usage"),
-        );
-
-    siv.add_layer(linear_layout);
-
-    siv.run();
+    launch_display_mode(interval);
 }
