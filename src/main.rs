0f0bb45e322c7f5c51c7bbf9ade2bd668f0688fe
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-10-30 22:04:06 +0100

    Init structure to handle log mode

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,42 @@
 use crate::display::launch_display_mode;
-use clap::{arg, command};
+use crate::log::launch_log_mode;
+use clap::{arg, command, value_parser};
 use std::time;
 
 pub mod display;
+pub mod log;
 
 fn main() {
     let matches = command!()
         .about("Agent collecting information about the OS on which it is installed")
-        .arg(arg!(-i --interval <INTERVAL> "Sets refresh interval (in seconds)"))
-        .arg(arg!(-d --display "Displays the output on the terminal"))
+        .arg(
+            arg!(-i --interval [INTERVAL] "Sets refresh interval (in seconds)")
+                .value_parser(value_parser!(u64))
+                .default_value("2"),
+        )
+        .arg(
+            arg!(<MODE>)
+                .help("What mode to run the program in")
+                .value_parser(["display", "log"]),
+        )
         .get_matches();
 
     let interval = matches
-        .get_one::<String>("interval")
-        .expect("Default interval is 2")
-        .parse::<u64>()
-        .unwrap();
-    let interval = time::Duration::from_secs(interval).as_secs();
+        .get_one::<u64>("interval")
+        .expect("Default interval is 2");
+    let interval = time::Duration::from_secs(*interval).as_secs();
 
-    launch_display_mode(interval);
+    match matches
+        .get_one::<String>("MODE")
+        .expect("'MODE' is required and parsing will fail if its missing")
+        .as_str()
+    {
+        "display" => {
+            launch_display_mode(interval);
+        }
+        "log" => {
+            launch_log_mode();
+        }
+        _ => unreachable!(),
+    }
 }
