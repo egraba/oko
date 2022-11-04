da7d100b73a3b42ec5c04b74983f0398a616bd19
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-11-04 20:40:44 +0100

    Add CpuInfo struct

--- src/oko.rs
+++ src/oko.rs
@@ -3,6 +3,35 @@ use sysinfo::{System, SystemExt};
 const NOT_RETRIEVED: &str = "NOT_RETRIEVED";
 
 #[derive(Debug)]
+pub struct CpuInfo {
+    arch: String,
+    model: String,
+    ncpus: usize,
+}
+
+impl CpuInfo {
+    pub fn new() -> Self {
+        CpuInfo {
+            arch: String::new(),
+            model: String::new(),
+            ncpus: 0,
+        }
+    }
+
+    pub fn arch(&self) -> &str {
+        &self.arch
+    }
+
+    pub fn model(&self) -> &str {
+        &self.model
+    }
+
+    pub fn ncpus(&self) -> usize {
+        self.ncpus
+    }
+}
+
+#[derive(Debug)]
 pub struct MemoryInfo {
     physmem: u64,
     swaptotal: u64,
@@ -48,9 +77,11 @@ impl OsInfo {
     }
 }
 
-pub fn collect_machine_info(memory_info: &mut MemoryInfo, os_info: &mut OsInfo) {
+pub fn collect_machine_info(cpu_info: &mut CpuInfo, memory_info: &mut MemoryInfo, os_info: &mut OsInfo) {
     let mut sys = System::new_all();
     sys.refresh_all();
+
+    cpu_info.ncpus = sys.cpus().len();
 
     memory_info.physmem = sys.total_memory();
     memory_info.swaptotal = sys.total_swap();
